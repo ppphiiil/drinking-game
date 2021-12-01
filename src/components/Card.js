e6a340074f843877d32354eca16c137b0d1a2d60
ppphiiil <info@philmeinwelt.com>
2021-12-01 22:36:36 +0100

    styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,7 +17,7 @@ const CardContainer = styled.div`
   width: 100vw;
   max-width: 500px;
   height: 100%;
-  padding-bottom: 10px;
+  padding: 0px 10px 10px 10px;
 `
 const CardStyle = styled.div`
   background-color: #0b132b;
