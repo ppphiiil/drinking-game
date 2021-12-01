211010ead45bfadae9573f187a549426c31f895f
ppphiiil <info@philmeinwelt.com>
2021-12-01 22:16:10 +0100

    delete url

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import { HashRouter} from 'react-router-dom'
 
 //MyContext API
 import {MyContextContainer} from './context/MyContextContainer'
-require( "dotenv" ).config();
-console.log("process.env.PUBLIC_URL",process.env.PUBLIC_URL)
 
 reactDOM.render(
  
